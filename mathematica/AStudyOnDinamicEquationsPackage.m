98e2cc29178602fce26df2b069dce180400406a2
Petro Kolosov <kolosovp94@gmail.com>
2022-01-18 20:40:59 +0100

    mathematica program updated

--- mathematica/AStudyOnDinamicEquationsPackage.m
+++ mathematica/AStudyOnDinamicEquationsPackage.m
@@ -1,18 +1,27 @@
 (* ::Package:: *)
 
-BeginPackage["arxiv160800801`"]
+BeginPackage["AStudyOnDynamicEquations`"]
 
-f::usage= "function which represents polynomial P."
-CoeffA::usage= "CoeffA[n,k] produces the coefficient A, see https://arxiv.org/abs/1603.02468."
-L::usage= "L[m, n, k] gives polynomial L, see https://arxiv.org/abs/1603.02468."
-P::usage= "P[m_, n_, a_, b_] gives the polynomial P, see https://arxiv.org/abs/1603.02468."
-sigma::usage= "time scale forward jump operator."
-timeScaleDifferenceX::usage= "gives a time scale difference of polynomial P in X."
-timeScaleDerivativeX::usage= "gives a time scale derivative of polynomial P in X."
-timeScaleDifferenceB::usage= "gives a time scale difference of polynomial P in B."
-timeScaleDerivativeB::usage= "gives a time scale derivative of polynomial P in B."
-timeScaleDerivaitveOddPower::usage= "gives a time scale derivative of polynomial X^{2m+1} in X."
-theorem::usage= "represents main theorem of the manuscript."
+A::usage= "A[n, k] returns the real coefficient A of non-negative integers n, k such that n <= k. 
+See https://kolosovpetro.github.io/pdf/AStudyOnDynamicEquations.pdf."
+
+L::usage= "L[m, n, k] returns the polynomial L of integers m, n, k. See https://kolosovpetro.github.io/pdf/AStudyOnDynamicEquations.pdf."
+
+P::usage= "P[m, x, b] returns the polynomial P of m, x and b. See https://kolosovpetro.github.io/pdf/AStudyOnDynamicEquations.pdf."
+
+sigma::usage= "Forward jump operator on time scales."
+
+timeScaleDifferenceX::usage= "Returns the partial time scale difference of polynomial P with respect to the varaible X."
+
+timeScaleDerivativeX::usage= "Returns the partial time scale derivative of polynomial P with respect to the varaible X."
+
+timeScaleDifferenceB::usage= "Returns the partial time scale difference of polynomial P with respect to the varaible B."
+
+timeScaleDerivativeB::usage= "Returns the partial time scale derivative of polynomial P with respect to the varaible B."
+
+timeScaleDerivaitveOddPower::usage= "Returns the partial time scale derivative of the polynomial X^{2m+1} with respect to the varaible X."
+
+theorem::usage= "Returns the partial dynamic equation identity such that is main result of the manuscript."
 
 Begin["`Private`"]
 
@@ -20,13 +29,11 @@ Unprotect[Power];
 Power[0|0., 0|0.] = 1;
 Protect[Power];
 
-f[m_, x_, b_] := P[m, x, b];
+A[n_, k_] := 0;
+A[n_, k_] := (2k + 1) * Binomial[2k, k] * Sum[A[n, j] * Binomial[j, 2k + 1] * (-1)^(j - 1) / (j - k) * BernoulliB[2j - 2k], {j, 2k + 1, n}] /; 0 <= k < n;
+A[n_, k_] := (2n + 1) * Binomial[2n, n] /; k == n;
 
-CoeffA[n_, k_] := 0;
-CoeffA[n_, k_] := (2k + 1) * Binomial[2k, k] * Sum[CoeffA[n, j] * Binomial[j, 2k + 1] * (-1)^(j - 1) / (j - k) * BernoulliB[2j - 2k], {j, 2k + 1, n}] /; 0 <= k < n;
-CoeffA[n_, k_] := (2n + 1) * Binomial[2n, n] /; k == n;
-
-L[m_, n_, k_] := Sum[CoeffA[m, r] * k^r * (n - k)^r, {r, 0, m}];
+L[m_, n_, k_] := Sum[A[m, r] * k^r * (n - k)^r, {r, 0, m}];
 P[m_, n_, b_] := Sum[L[m, n, k], {k, 0, b - 1}];
 
 sigma[x_] := x + 1;
@@ -46,6 +53,9 @@ theorem[m_] := Expand[timeScaleDerivativeX[m, Global`x, sigma[Global`x]] + timeS
 End[ ]
 
 EndPackage[ ]
+
+
+
 
 
 
